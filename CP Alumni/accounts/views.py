aab5e7971b0de0d51431cd158d4b79d7964abeb4
mjblaha22 <mjblaha22@gmail.com>
2020-09-15 12:06:39 -0400

    added remake views accounts/views.py

--- CP Alumni/accounts/views.py
+++ CP Alumni/accounts/views.py
@@ -9,3 +9,37 @@ class SignUpView(generic.CreateView):
     success_url = reverse_lazy('login')
     template_name = 'registration/signup.html'
 # Create your views here.
+
+# remake stuff
+class UsersViewSet(viewsets.ModelViewSet):
+    queryset = Users.objects.all()
+    serializer_class = UsersSerializer
+
+class EventsViewSet(viewsets.ModelViewSet):
+    queryset = Events.objects.all()
+    serializer_class = EventsSerializer
+
+class MessagesViewSet(viewsets.ModelViewSet):
+    queryset = Messages.objects.all()
+    serializer_class = MessagesSerializer
+
+class BusinessesViewSet(viewsets.ModelViewSet):
+    queryset = Businesses.objects.all()
+    serializer_class = BusinessesSerializer
+
+class LocationsViewSet(viewsets.ModelViewSet):
+    queryset = Locations.objects.all()
+    serializer_class = LocationsSerializer
+
+class EmailsViewSet(viewsets.ModelViewSet):
+    queryset = Emails.objects.all()
+    serializer_class = EmailsSerializer
+
+class ResponsesViewSet(viewsets.ModelViewSet):
+    queryset = Responses.objects.all()
+    serializer_class = ResponsesSerializer
+
+class User_BusinessViewSet(viewsets.ModelViewSet):
+    queryset = User_Business.objects.all()
+    serializer_class = User_BusinessSerializer
+
