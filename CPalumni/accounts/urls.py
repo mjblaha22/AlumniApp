66e544058d6ea3c6ca992abd975078216d303fff
mjblaha22 <mjblaha22@gmail.com>
2020-09-16 14:42:49 -0400

    added more b.e.

--- CPalumni/accounts/urls.py
+++ CPalumni/accounts/urls.py
@@ -1,8 +1,19 @@
-from django.urls import path
+from django.urls import path, include
 
-from .views import SignUpView
+from .views import SignUpView, UsersViewSet, EventsViewSet, UsersViewSet, LocationsViewSet, BusinessesViewSet, EmailsViewSet, MessagesViewSet, ResponsesViewSet, User_BusinessViewSet
+from rest_framework.routers import DefaultRouter
 
-
-urlpatterns = [
-    path('signup/', SignUpView.as_view(), name='signup'),
-]
+router = DefaultRouter()
+router.register(r'signup', SignUpView, basename='signup')
+router.register(r'user', UsersViewSet, basename='user')
+router.register(r'emails', EmailsViewSet, basename='emails')
+router.register(r'events', EventsViewSet, basename='events')
+router.register(r'locations', LocationsViewSet, basename='locations')
+router.register(r'businesses', BusinessesViewSet, basename='businesses')
+router.register(r'messages', MessagesViewSet, basename='messages')
+router.register(r'responses', ResponsesViewSet, basename='responses')
+router.register(r'userbusiness', User_BusinessViewSet, basename='userbusiness')
+urlpatterns = router.urls
+# urlpatterns = [
+#     path('signup/', SignUpView.as_view(), name='signup'),
+# ]
