acaab5a9e75db0a64bfe3bed32e21e5a98d19c16
mjblaha22 <mjblaha22@gmail.com>
2020-09-16 21:28:47 -0400

    working again!!

--- CPalumni/accounts/urls.py
+++ CPalumni/accounts/urls.py
@@ -3,17 +3,26 @@ from rest_framework import viewsets
 from .views import SignUpView, UsersViewSet, EventsViewSet, UsersViewSet, LocationsViewSet, BusinessesViewSet, EmailsViewSet, MessagesViewSet, ResponsesViewSet, User_BusinessViewSet
 from rest_framework.routers import DefaultRouter
 
-# urlpatterns = [
-#     path('signup/', SignUpView.as_view(), name='signup'),
-# ]
-router = DefaultRouter()
-router.register(r'signup', SignUpView.as_view(), basename='signup')
-router.register(r'user', UsersViewSet, basename='user')
-router.register(r'emails', EmailsViewSet, basename='emails')
-router.register(r'events', EventsViewSet, basename='events')
-router.register(r'locations', LocationsViewSet, basename='locations')
-router.register(r'businesses', BusinessesViewSet, basename='businesses')
-router.register(r'messages', MessagesViewSet, basename='messages')
-router.register(r'responses', ResponsesViewSet, basename='responses')
-router.register(r'userbusiness', User_BusinessViewSet, basename='userbusiness')
-urlpatterns = router.urls
+urlpatterns = [
+    path('signup/', SignUpView.as_view(), name='signup'),
+    path('user/', UsersViewSet, name='user'),
+    path('emails', EmailsViewSet, name='emails'),
+    path('events', EventsViewSet, name='events'),
+    path('locations', LocationsViewSet, name='locations'),
+    path('businesses', BusinessesViewSet, name='businesses'),
+    path('messages', MessagesViewSet, name='messages'),
+    path('responses', ResponsesViewSet, name='responses'),
+    path('userbusiness', User_BusinessViewSet, name='userbusiness'),
+
+]
+# router = DefaultRouter()
+# router.register(r'signup', SignUpView.as_view(template_name='home.html'), basename='signup')
+# router.register(r'user', UsersViewSet, basename='user')
+# router.register(r'emails', EmailsViewSet, basename='emails')
+# router.register(r'events', EventsViewSet, basename='events')
+# router.register(r'locations', LocationsViewSet, basename='locations')
+# router.register(r'businesses', BusinessesViewSet, basename='businesses')
+# router.register(r'messages', MessagesViewSet, basename='messages')
+# router.register(r'responses', ResponsesViewSet, basename='responses')
+# router.register(r'userbusiness', User_BusinessViewSet, basename='userbusiness')
+# urlpatterns = router.urls
